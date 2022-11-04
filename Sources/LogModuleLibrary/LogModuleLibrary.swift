74e5ee28a917e2f77f377ba65e681d51f51ac1cf
許湘潔 <jessiehsu@candyhouse.co>
2022-11-04 10:07:55 +0800

    commit log

--- Sources/LogModuleLibrary/LogModuleLibrary.swift
+++ Sources/LogModuleLibrary/LogModuleLibrary.swift
@@ -1,6 +1,17 @@
-public struct LogModuleLibrary {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
+import Foundation
+import os.log
+public class lg {
+  public static func debug(_ items: Any..., file: String = #file, function: String = #function,
+         line: Int = #line, tag: String = "WEB =>") {
+    #if DEBUG
+    let fileName = (file as NSString).lastPathComponent
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "HH:mm:ss"
+    dateFormatter.timeZone = TimeZone(abbreviation: "Asia/Taipei")
+    dateFormatter.locale = Locale(identifier: "ja_JP")
+    let ismain = (Thread.isMainThread) ? "主線":"副線"
+    let content = "\(tag) \(dateFormatter.string(from: Date())) \(items) \(fileName):\(line) \(ismain)"
+    print(content)
+    #endif
+  }
 }
